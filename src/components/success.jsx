049d4134119c3a8034bb9ad4cd36271fc10d0bcf
anandhakumarmca@gmail.com <accetanandhmca@gmail.com>
2024-03-28 14:56:26 +0530

    changes updated

--- src/components/success.jsx
+++ src/components/success.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCheckCircle } from "react-icons/fa";
 
 const SuccessPage = ({ message }) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center py-5 md:py-0">
       <div className="container mx-auto flex items-center justify-center">
         <div className="w-full md:w-2/3 lg:w-1/2 xl:w-2/5 bg-white rounded-lg shadow-lg p-8">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">Success!</h1>
+          <div className="flex items-center justify-center mb-4">
+            <FaCheckCircle className="text-green-500 text-5xl mr-2" />
+            <h1 className="text-3xl font-bold text-gray-800">Success!</h1>
+          </div>
           <p className="text-gray-700 mb-8 text-center">{message}</p>
           <Link
             to="/"
